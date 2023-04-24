f0a838d98f434ef919ad1b9688f2448ae9df3c3a
Nubthong Worathong <nubthongwor@gmail.com>
2023-04-24 08:34:14 +0700

    🫡 modified frontend

--- front-end/my-react-app/src/App.js
+++ front-end/my-react-app/src/App.js
@@ -1,13 +1,18 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Nav from './components/Nav'
+
 // import Profile from './components/Profile'
 import WebcamImage from './components/WebcamImage'
 import Webcam from "react-webcam";
 export default function App() {
   return (
+    <>
+    <Nav/>
     <div className="container mt-5">
       {/* <Profile /> */}
       <WebcamImage />
     </div>
+    </>
   )
 }
